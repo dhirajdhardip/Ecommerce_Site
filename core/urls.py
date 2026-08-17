ce85fd9ff8ac770d689e17ea5f204872f09eaaab
Dhiraj Dhar Dip <dhirajdhardip@gmail.com>
2026-08-18 05:04:35 +0600

    Serve media files in production

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
-    path('user/', include('users.urls')),
-    path('cart/', include('cart.urls')),
-    path('wishlist/', include('wishlist.urls')),
-    path('compare/', include('compare.urls')),
-    path('checkout/', include('orders.urls')),
+    path("admin/", admin.site.urls),
+
+    path("", include("store.urls")),
+    path("user/", include("users.urls")),
+    path("cart/", include("cart.urls")),
+    path("wishlist/", include("wishlist.urls")),
+    path("compare/", include("compare.urls")),
+    path("checkout/", include("orders.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
