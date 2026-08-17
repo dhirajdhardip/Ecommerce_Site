be98bb2d330d1bc9e204e0209fdb5b8e746f6064
Dhiraj Dhar Dip <dhirajdhardip@gmail.com>
2026-08-18 03:18:01 +0600

    Fix production build and admin setup

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
-echo "==> Deployment Build Completed Successfully!"
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status
 set -o errexit
+
+echo "==> Installing Dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "==> Collecting Static Files..."
+python manage.py collectstatic --no-input
+
+echo "==> Running Database Migrations..."
+python manage.py migrate --no-input
+
 echo "==> Creating Production Admin User..."
 
 python manage.py shell -c "
@@ -37,14 +46,5 @@ if username and email and password:
 else:
     print('Production admin variables not configured.')
 "
-echo "==> Installing Dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "==> Collecting Static Files..."
-python manage.py collectstatic --no-input
-
-echo "==> Running Database Migrations..."
-python manage.py migrate --no-input
 
 echo "==> Deployment Build Completed Successfully!"
